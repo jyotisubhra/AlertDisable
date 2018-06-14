3fa7c2fe2d959b1a3e66a32f1de2aebe3564b963
jyotisubhra <33259027+jyotisubhra@users.noreply.github.com>
2018-06-14 14:31:01 +0530

    as per new structure

--- target/site/cucumber-pretty/report.js
+++ target/site/cucumber-pretty/report.js
@@ -7,41 +7,113 @@ formatter.feature({
   "keyword": "Feature"
 });
 formatter.scenario({
-  "line": 4,
-  "name": "Disable Nagios Alert",
+  "line": 9,
+  "name": "schedule downtime for all services",
   "description": "",
-  "id": "disable-nagios-alert-for-all-hosts-scenario;disable-nagios-alert",
+  "id": "disable-nagios-alert-for-all-hosts-scenario;schedule-downtime-for-all-services",
   "type": "scenario",
   "keyword": "Scenario",
   "tags": [
     {
-      "line": 3,
+      "line": 8,
       "name": "@run"
     }
   ]
 });
 formatter.step({
-  "line": 5,
-  "name": "Open IE and start application",
+  "line": 10,
+  "name": "Open IE and start the application",
   "keyword": "Given "
 });
 formatter.step({
-  "line": 6,
-  "name": "I Select Hosts link",
+  "line": 11,
+  "name": "Iterate the Hostlists",
   "keyword": "When "
 });
+formatter.step({
+  "line": 12,
+  "name": "Select \"hostlink\" for each host",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 13,
+  "name": "select schedule downtime for all services for the \"hsotname\"",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 14,
+  "name": "Enter Comment as \"comment\"",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 15,
+  "name": "Enter Start Date as \"startDate\"",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 16,
+  "name": "Enter End Date as \"End Date\"",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 17,
+  "name": "Click on Commit as \"Enable\"",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 18,
+  "name": "Click on Hosts link",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 19,
+  "name": "click on Downtime link",
+  "keyword": "Then "
+});
 formatter.match({
-  "location": "StepDef.Open_IE_and_start_application()"
+  "location": "StepDef.open_IE_and_start_the_application()"
 });
 formatter.result({
-  "duration": 19373629145,
+  "duration": 166232511,
   "status": "passed"
 });
 formatter.match({
-  "location": "StepDef.I_Select_Hosts_link()"
+  "location": "StepDef.iterate_the_Hostlists()"
 });
 formatter.result({
-  "duration": 42873122243,
+  "duration": 598447450287,
   "status": "passed"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
 });
 });
